fe476f033003b62f0d84d2bf89c00235ded683a9
abdul qudoos <abdulqudoospk737@gmail.com>
2024-04-13 21:34:54 +0500

    practice two js problem

---
+++ 16_js_problem.js
@@ -0,0 +1,16 @@
+// Problem No.15
+// Find the min val in array
+
+const findMin = (arr) => {
+  arr = arr.sort((a, b) => {
+    if(a < b) return -1;
+  });
+  return arr[0]
+}; 
+
+// const findMin = (arr) => {
+//   return Math.min(...arr);
+// };
+
+console.log(findMin([5, 10, 2, 8]));
+console.log(findMin([5, -3, 0, 12, -7]));
---
+++ 17_js_problem.js
@@ -0,0 +1,16 @@
+// Problem No.15
+// Write a function to convert a string to camelCase & snack_case
+
+const toCamelCase = (str)=>{
+    str = str.trim().split(' ')
+    str = str.map((curElem, index)=>{
+        if(index === 0){
+            return curElem.toLowerCase()
+        } else{
+            return curElem.charAt(0).toUpperCase() + curElem.slice(1).toLowerCase()
+        }
+    })
+    return str.join('')
+}
+
+console.log(toCamelCase("hello world abdu"))
